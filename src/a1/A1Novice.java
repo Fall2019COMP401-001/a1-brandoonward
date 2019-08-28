5ede38819d1eae73ce8f21927f68b7384eefb003
Brandon Ward <Brandon Ward@LAPTOP-1PR3AGA6>
2019-08-28 14:28:46 -0400

    First attempt at A1Novice.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -6,9 +6,54 @@ public class A1Novice {
 
 	public static void main(String[] args) {
 		
+		//creating scanner to read for input from console
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		//allowing the next integer input by the user to be recorded by scanner
+		int count = scan.nextInt() + 1;
+		
+		//creating a string array which will hold the names of the customers, length determined by first integer
+		String[] customers = new String[count];
+		
+		//reading values into the string array
+		for (int i=0; i<=customers.length; i++) {
+			
+			//creating two string arrays, one which will hold the first names and other which will hold last names. Length based on amount of customers.
+			String[] firstName = new String[customers.length];
+			
+			//first name will be next string input through scanner
+			firstName[i] = scan.next();
+			
+			// same for last name
+			String[] lastName = new String [customers.length];
+			lastName[i] = scan.next();
+			
+			//creating an integer that will hold the number of items for the next for loop.
+			int numberOfItems = scan.nextInt();
+			
+			//creating an double array which will hold the total for each customer.
+			double[] total = new double[numberOfItems];
+			
+			//new for loop which will process the total cost for each customer
+			for (int i1=0; i1<numberOfItems; i1++) {
+				
+				//scanning for the number of the item, name, and price of the item.
+				int numSpecificItem = scan.nextInt();
+				String nameOfItem = scan.next();
+				double price = scan.nextDouble();
+				
+				//calculating the total for said customer.
+				total[i] = price * numSpecificItem + total[i];
+				
+			}
+			
+			// printing out the first initial of their name by using the charAt feature and picking the first letter
+			// then assembling the rest by using the last name and a series of concatenations along with the total.
+			System.out.print(firstName[i].charAt(0) + "." + lastName[i] + " " + total[i]);
+		}
+		
+		// closing the scanner as it is no longer used.
+		scan.close();
 		
 	}
 }
